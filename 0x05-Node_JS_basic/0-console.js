b50cd1965c22ace6e6990c0e9de30a2a37955c57
Geepeey <harshmallins@gmail.com>
2023-08-21 23:39:54 +0000

    Display message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+/**
+ * Executing basic javascript with Node JS
+ * 
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ */
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
